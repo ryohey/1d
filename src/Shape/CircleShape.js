307edc0cc8495e1753f347a5243a9817fba725d1
ryohey <info@codingcafe.jp>
2017-02-02 23:06:23 +0900

    support resizing CircleShape

--- src/Shape/CircleShape.js
+++ src/Shape/CircleShape.js
@@ -1,14 +1,25 @@
 import React from "react"
 import Shape from "./Shape"
+import _ from "lodash"
+
+function toPoint(value) {
+  if (_.isNumber(value)) {
+    return {
+      x: value,
+      y: value
+    }
+  }
+  return value
+}
 
 export default class CircleShape extends Shape {
   constructor(pos = { x: 0, y: 0 }, radius = 0) {
     super(pos)
-    this.radius = radius
+    this.radius = toPoint(radius)
   }
 
   resize(size) {
-    // TODO: リサイズと楕円の実装
+    this.radius = size
   }
 
   render() {
@@ -17,8 +28,9 @@ export default class CircleShape extends Shape {
       cursor="move"
       onMouseOver={e => mouseHandler.onMouseOver(e, this)}
       onMouseDown={e => mouseHandler.onMouseDown(e, this)}>
-      <circle
-        r={radius}
+      <ellipse
+        rx={radius.x}
+        ry={radius.y}
         stroke={brush.stroke || "none"}
         strokeWidth={brush.strokeWidth || 1}
         fill={brush.fill || "none"}
